e1eb1dbb73ad3c9fbe81741a616becf5b3366f38
陈佳能 <751855556@qq.com>
2018-12-27 20:52:07 +0800

    跳转修复链接

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -27,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim('chenjianeng0201@gmail.com')));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
